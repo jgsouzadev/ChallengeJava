60bee1b423f4e56d4f43ae29043a4fc399592462
Joao Guilherme de Souza <joao.souza@fcamara.com.br>
2021-09-09 15:49:09 -0300

    Project Setup

---
+++ src/main/java/fc/desafio/tecnico/rest/EndpointParaODesafioApplication.java
@@ -0,0 +1,13 @@
+package fc.desafio.tecnico.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointParaODesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointParaODesafioApplication.class, args);
+	}
+
+}
