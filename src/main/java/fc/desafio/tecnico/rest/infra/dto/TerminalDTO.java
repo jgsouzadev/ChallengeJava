3c9d79e4d5a0eaa9caca1895a6c9bffdefec7681
Joao Guilherme de Souza <joao.souza@fcamara.com.br>
2021-09-09 23:11:17 -0300

    feat: add DTO

---
+++ src/main/java/fc/desafio/tecnico/rest/infra/dto/TerminalDTO.java
@@ -0,0 +1,32 @@
+package fc.desafio.tecnico.rest.infra.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import fc.desafio.tecnico.rest.domain.entity.Terminal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class TerminalDTO {
+
+	private String serial;
+	private String model;
+	private Integer sam;
+	private String ptid;
+	private Integer plat;
+	private String version;
+	private Integer mxr;
+	private Integer mxf;
+	@JsonProperty(value = "VERMF")
+	private String vermf;
+
+	public Terminal conversorDTO(TerminalDTO terminalDTO) {
+		return Terminal.builder().withSerial(terminalDTO.getSerial()).withModel(terminalDTO.getModel())
+				.withMxf(terminalDTO.getMxf()).withMxr(terminalDTO.getMxr()).withPlat(terminalDTO.getPlat())
+				.withPtid(terminalDTO.getPtid()).withVersion(terminalDTO.getVersion()).withVermf(terminalDTO.getVermf())
+				.withSam(terminalDTO.getSam()).build();
+	}
+}
